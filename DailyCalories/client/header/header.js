21d0edad1afad25f8eb13a9e84de853debaea995
Brady Wright <Pro75357@gmail.com>
2017-12-29 17:38:22 -0800

    pills highlight

--- DailyCalories/client/header/header.js
+++ DailyCalories/client/header/header.js
@@ -1,9 +1,22 @@
+
+const navs = ["daily","database","importExport"];
+
+Session.set('activeNav',navs[0])
+
 Template.header.helpers({
+    navs(){
+        return navs;
+    },
     user(){
         return Meteor.user()
     },
     uservomit(){
         return JSON.stringify(Meteor.user(),null,2)
+    },
+    maybeActive(nav){
+        if (Session.equals('activeNav',nav)){
+            return "active"
+        }
     }
 
 });
@@ -12,4 +25,7 @@ Template.header.events({
    'click #logout'(){
        Meteor.logout()
    },
+    'click a'(event){
+       Session.set('activeNav',event.target.id)
+    }
 });
